f05d23893b0b12d4e99338e3af8a2df3f534c744
LingGrani <luthfiangger2510@gmail.com>
2024-03-18 20:28:54 +0700

    Add Comment

--- script/script.js
+++ script/script.js
@@ -1,11 +1,11 @@
 function klik() {
   alert("PERINGATAN")
-}
+} //fungsi pop up sederhana ketika button ditekan
 
 function updateJam() {
   var currentTime = new Date();
-
   document.getElementById("realTime").innerHTML = currentTime;
-}
-setInterval(updateJam, 1000);
+} //fungsi memasukan waktu kedalam html element
+
+setInterval(updateJam, 1000); // menjalankan fungsi setiap 1000ms/1 detik
 updateJam();
